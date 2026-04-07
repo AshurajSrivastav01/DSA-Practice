9f39188dcfb739f557a5bb776c3320a35d45ecd8
AshurajSrivastav01 <ashuraj.srivastav01@gmail.com>
2026-04-07 08:04:41 +0530

    Just learnd about bubble sort

---
+++ day01/bubble_sorting.js
@@ -0,0 +1,17 @@
+console.log('Bubble Sorting');
+
+let arr = [5, 1, 4, 2, 8];
+
+for(let i=0; i <arr.length; i++) {
+    for (let j = 0; j <arr.length-1; j++) {
+        if(arr[j] > arr[j+1]){
+            // Sawping
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+
+        console.log(arr.length - 1 );
+    }
+}
+console.log(arr);
